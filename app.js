ffc92b8af8bd87d8e256691d816df4235407c749
HerrKise <oskarwilde777@gmail.com>
2022-02-22 21:18:44 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const config = require("config");
+const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
+
+const app = express();
+
+const PORT = config.get("port") ?? 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api", routes);
+
+/* if (process.env.NODE_ENV === "production") {
+    console.log("production");
+} else {
+    console.log("development");
+} */
+
+async function start() {
+    try {
+        mongoose.connection.once("open", () => {
+            initDatabase();
+        });
+        await mongoose.connect(config.get("mongoUri"));
+        app.listen(PORT, () => {
+            console.log(
+                chalk.green(`Server has been started on port ${PORT}...`)
+            );
+        });
+    } catch (error) {
+        console.log(chalk.red(error.message));
+        process.exit(1);
+    }
+}
+
+start();
